d38f0c5e1feafbd87258c29a9cc7c95aaf3fa221
Uzair Fasih <uzair.fassi@gmail.com>
2022-11-13 03:07:09 -0500

    Added dp algorithm for task 5

---
+++ tasks/task5.cpp
@@ -0,0 +1,74 @@
+/**
+ *
+ *  [Problem 2.]
+ *
+ *  Given a matrix A of m x n integers (non-negative) representing the predicted
+ *  prices of m stocks for n days and an integer k (positive), find a sequence
+ *  of at most k transactions that gives maximum profit.
+ *
+ *  [Hint :- Try to solve for k = 2 first and then expand that solution.]
+ *
+ *  [Alg 5.]
+ *
+ *  Design a Θ(m * n^2 * k) time dynamic programming algorithm for solving
+ *  Problem 2.
+ *
+ *  [Task 5.]
+ *
+ *  Give an implementation of Alg 5.
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxProfitsFromkTransactions(vector<vector<int>>& stocks, int k) {
+  if (stocks.size() == 0)
+    return 0;
+
+  int m = stocks.size(), n = stocks[0].size();
+
+  vector<vector<int>> dp(k, vector<int>(n));
+
+  // Regardless of the number of transactions, there is no
+  // profit to be made on the first day.
+
+  for (int t = 0; t < k; t++)
+    dp[t][0] = 0;
+
+  // Regardless of the number of days under consideration, there
+  // is no profit to be made when there are no transactions.
+
+  for (int i = 0; i < n; i++)
+    dp[0][i] = 0;
+
+  for (int t = 1; t < k; t++) {
+    for (int i = 1; i < n; i++) {
+      // Either no transaction on day i or we sell some stock on day i and buy
+      // it on some prior day j. We require the maximum of such transaction
+
+      int maxPossibleProfit = INT_MIN;
+
+      for (int s = 0; s < m; s++) {
+        for (int j = 0; j < i; j++) {
+          int currProfit = stocks[s][i] - stocks[s][j] + dp[t - 1][j];
+          maxPossibleProfit = max(maxPossibleProfit, currProfit);
+        }
+      }
+
+      dp[t][i] = max(dp[t][i - 1], maxPossibleProfit);
+    }
+  }
+
+  return dp[k - 1][n - 1];
+}
+
+int main() {
+  vector<vector<int>> stocks = {{10, 20, 13, 8, 9, 12, 4},
+                                {7, 1, 5, 3, 6, 4, 19},
+                                {7, 6, 4, 3, 1, 10, 3}};
+  int k = 4;
+
+  int sol = maxProfitsFromkTransactions(stocks, k);
+  cout << sol << endl;
+}
