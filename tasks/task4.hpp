bd2305a2aea6f8c2d3352e114b1627fdfb66d590
Uzair Fasih <uzair.fassi@gmail.com>
2022-11-16 21:20:20 -0500

    Completed task4

--- tasks/task4.hpp
+++ tasks/task4.hpp
@@ -22,7 +22,10 @@ using namespace std;
  *
  */
 
-int transactKTimes(vector<vector<int>>& stocks, int start, int k) {
+int transactKTimes(vector<vector<int>>& stocks,
+                   vector<answer>& ledger,
+                   int start,
+                   int k) {
   if (k <= 0)
     return 0;
 
@@ -35,8 +38,16 @@ int transactKTimes(vector<vector<int>>& stocks, int start, int k) {
         auto prices = stocks[l];
 
         // Either do a transaction or don't
-        int p1 = prices[j] - prices[i] + transactKTimes(stocks, j, k - 1);
-        int p2 = transactKTimes(stocks, j, k);
+        int p1 =
+            prices[j] - prices[i] + transactKTimes(stocks, ledger, j, k - 1);
+        int p2 = transactKTimes(stocks, ledger, j, k);
+
+        if (p1 > p2 && p1 > profit && p1 > ledger[k].profit) {
+          ledger[k].profit = p1;
+          ledger[k].stock = l;
+          ledger[k].buyDay = i;
+          ledger[k].sellDay = j;
+        }
 
         profit = max({profit, p1, p2});
       }
@@ -47,8 +58,20 @@ int transactKTimes(vector<vector<int>>& stocks, int start, int k) {
 }
 
 void buyAndSellFromkTransactions() {
-  pair<int, vector<vector<int>>> input = getInputWithK();
+  pair<int, vector<vector<int>>> input = {
+      3, vector<vector<int>>{{10, 20, 13, 8, 9, 12, 4},  // 10, 20
+                             {7, 1, 5, 3, 6, 4, 19},     // 1, 19
+                             {7, 6, 4, 3, 1, 10, 30}}};  // getInputWithK();
   auto stocks = input.second;
   int k = input.first;
-  cout << transactKTimes(stocks, 0, k) << endl;
+
+  vector<answer> ledger(k + 1, answer());
+  cout << transactKTimes(stocks, ledger, 0, k) << endl;
+
+  for (int i = k; i >= 1; i--) {
+    auto t = ledger[i];
+
+    if ((i == 1) || (i > 1 && t.buyDay != ledger[i - 1].buyDay))
+      cout << t.stock << " " << t.buyDay << " " << t.sellDay << endl;
+  }
 }
