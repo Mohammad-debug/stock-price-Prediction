1d52317fa2bded406a58c19873e9148d4de98bc8
Uzair Fasih <uzair.fassi@gmail.com>
2022-11-21 19:47:58 -0500

    Latest

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include "./tasks/task6b.hpp"
 #include "./tasks/task7.hpp"
 #include "./tasks/task8.hpp"
+#include "./tasks/task9b.hpp"
 
 using namespace std;
 
@@ -20,7 +21,8 @@ int main(int argc, char* argv[]) {
   }
 
   string task = argv[1];
-  vector<string> tasks = {"1", "2", "3a", "3b", "4", "5", "6a", "6b", "7", "8"};
+  vector<string> tasks = {"1",  "2",  "3a", "3b", "4",  "5",
+                          "6a", "6b", "7",  "8",  "9a", "9b"};
 
   auto it = std::find(tasks.begin(), tasks.end(), task);
   auto index = distance(tasks.begin(), it);
@@ -60,6 +62,12 @@ int main(int argc, char* argv[]) {
     case 9:
       transactKTimesWithCooldownDP();
       break;
+    case 10:
+      transactKTimesWithCooldownDP();
+      break;
+    case 11:
+      transactKTimesWithCooldownDPOptimizedTabulation();
+      break;
     default:
       cout << "Bad input.\nTask not found" << endl;
       return -1;
@@ -68,6 +76,6 @@ int main(int argc, char* argv[]) {
   auto duration =
       std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 
-  // cout << duration.count() << endl;
+  cout << duration.count() << endl;
   return 0;
 }
